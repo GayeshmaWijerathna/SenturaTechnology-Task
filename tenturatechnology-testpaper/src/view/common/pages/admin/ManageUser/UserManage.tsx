f5516ddc44453ce49e68bd00c2775fe4a34d1caa
GayeshmaWijerathna <109783810+GayeshmaWijerathna@users.noreply.github.com>
2024-02-03 10:45:31 +0530

    updated

--- tenturatechnology-testpaper/src/view/common/pages/admin/ManageUser/UserManage.tsx
+++ tenturatechnology-testpaper/src/view/common/pages/admin/ManageUser/UserManage.tsx
@@ -1,9 +1,15 @@
-import {Component} from "react";
+import React, {Component} from "react";
+
+
 
 export class UserManage extends Component {
     render() {
         return (
             <>
+                    <h1 className="text-4xl text-blue-950">Manage User Data</h1>
+
+                <text className="placeholder:">name</text>
+
 
             </>
         );
